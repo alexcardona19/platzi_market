537030f1e093f896c9db1ee16b1bce7616a3202f
alexcardona19 <alexanderca255@gmail.com>
2021-10-30 17:02:49 -0500

    Configuración de Spring boot

--- src/main/java/com/platzi/market/HolaMundoController.java
+++ src/main/java/com/platzi/market/HolaMundoController.java
@@ -1,6 +1,5 @@
 package com.platzi.market;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
